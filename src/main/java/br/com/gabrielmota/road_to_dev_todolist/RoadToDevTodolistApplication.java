14a786efe4eb8d92c657c95bf62416c57b00b87f
Mota01G <gabrielfillipe92@gmail.com>
2024-11-20 18:57:08 -0300

    Criando a entidade To do

---
+++ src/main/java/br/com/gabrielmota/road_to_dev_todolist/RoadToDevTodolistApplication.java
@@ -0,0 +1,13 @@
+package br.com.gabrielmota.road_to_dev_todolist;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoadToDevTodolistApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoadToDevTodolistApplication.class, args);
+	}
+
+}
