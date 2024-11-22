2554546cc8fa8f42a106d0088bdd8b70ec40a24f
Mota01G <gabrielfillipe92@gmail.com>
2024-11-21 22:12:56 -0300

    Definição do domínio - Controller

--- src/main/java/br/com/gabrielmota/road_to_dev_todolist/service/TodoService.java
+++ src/main/java/br/com/gabrielmota/road_to_dev_todolist/service/TodoService.java
@@ -10,30 +10,29 @@ import br.com.gabrielmota.road_to_dev_todolist.repository.TodoRepository;
 
 @Service
 public class TodoService {
-    
     private TodoRepository todoRepository;
     
     public TodoService(TodoRepository todoRepository) {
         this.todoRepository = todoRepository;
     }
      
-    List<Todo> create(Todo todo) {
+    public List<Todo> create(Todo todo) {
         todoRepository.save(todo);
         return list();
     }
-    
-    List<Todo> list() {
+
+    public List<Todo> list() {
         Sort sort = Sort.by("prioridade").descending().and(
             Sort.by("nome").ascending());
         return todoRepository.findAll(sort);
     }
 
-    List<Todo> uptate(Todo todo) {
+    public List<Todo> uptate(Todo todo) {
         todoRepository.save(todo);
         return list();
     }
 
-    List<Todo> delete(Long id) {
+    public List<Todo> delete(Long id) {
         todoRepository.deleteById(id);
         return list();
     }
